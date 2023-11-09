a336cc8042f5220161545d5c6fc53eb85e24e8fa
minhtruc <truc0979454023@gmail.com>
2023-11-09 14:00:58 +0700

    Bai 2.5

---
+++ src/Blog.Core/Domain/Content/Post.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Blog.Core.Domain.Content
+{
+    [Table("Posts")]//Create table database
+    [Index(nameof(Slug), IsUnique = true)]// để sử dụng install package Entity, công dụng giúp trường Slug là trường dữ liệu không lặp lại
+    public class Post
+    {
+        [Key]
+        public Guid Id { get; set; } //Type Guid is Unique 128 bit
+
+        [Required]
+        [MaxLength(250)]
+        public required string Name { get; set; }
+
+        [Required]
+        [Column(TypeName = "varchar(250)")]
+        public required string Slug { get; set; }
+
+        [MaxLength(500)]
+        public string? Description { get; set; }
+
+        [Required]
+        public Guid CategoryId { get; set; }
+
+        [MaxLength(500)]
+        public string? Thumbmail { get; set; }
+
+        public string? Content { get; set; }
+
+        [MaxLength(500)]
+        public Guid AuthorUserId { get; set; }
+
+        [MaxLength(128)]
+        public string? Source { get; set; }
+
+        [MaxLength(250)]
+        public string? Tags { get; set; }
+
+        [MaxLength(160)]
+        public string? SeoDescription { get; set; }
+
+        public int ViewCount { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime? DateModified { get; set; }
+        public bool IsPaid { get; set; }
+        public double RoyaltyAmount { get; set; }
+        public PostStatus Status { get; set; }
+    }
+
+    public enum PostStatus
+    {
+        Draft = 1,
+        Canceled = 2,
+        WaitingForApproval = 3,
+        Rejected = 4,
+        WaitingForPublic = 5,
+        Published = 6
+    }
+}
